26ac62c3737a8a174153f2a79b05a0cbb031649a
william <william.del.rosario@iceshop.nl>
2020-01-31 17:50:19 +0100

    Key creation

--- services/cache.js
+++ services/cache.js
@@ -4,6 +4,16 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
     console.log('IM ABOUT TO RUN A QUERY');
+    console.log(this.getQuery());
+    console.log(mongoose.Collection.name);
+
+
+    const key = Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name
+    });
+
+    console.log(key);
+    console.dir(key);
 
     return exec.apply(this, arguments)
 };
