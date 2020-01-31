fb3fada2ac2053827f773ac2f1852fac6430b5a1
william <william.del.rosario@iceshop.nl>
2020-01-31 17:53:36 +0100

    Restoring redis config

--- services/cache.js
+++ services/cache.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
-    console.log('IM ABOUT TO RUN A QUERY');
-    console.log(this.getQuery());
-    console.log(mongoose.Collection.name);
-
-
+    console.log('IM ABOUT TO RUN A QUERY')
     const key = Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
     });
-
-    console.log(key);
-    console.dir(key);
 
     return exec.apply(this, arguments)
 };
