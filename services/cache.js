8a9ab5abd67aea45e7fde21fca9f5c7821d353bc
william <william.del.rosario@iceshop.nl>
2020-01-31 18:38:17 +0100

    Toggleable cache

--- services/cache.js
+++ services/cache.js
@@ -7,7 +7,16 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function () {
+    this.useCache = true;
+
+    return this;
+};
+
 mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+        return exec.apply(this, arguments)
+    }
     const key = JSON.stringify(Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
     }));
@@ -24,13 +33,6 @@ mongoose.Query.prototype.exec = async function () {
 
     // Otherwise, issue the query and store the result in redis
     const result = await exec.apply(this, arguments);
-
-    console.log(result);  // type is mongoose document, ModelInstance, not a js object
-    console.log(result.validate);
-    // [Function: wrappedPointCut] {
-    //     '$originalFunction': '$__original_validate',
-    //            '$isWrapped': true
-    // }
 
     // Set result as a string in redis
     client.set(key, JSON.stringify(result));
