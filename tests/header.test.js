cad3a5e0d326381b441a176d410205294d0628de
william <william.del.rosario@iceshop.nl>
2020-02-01 14:29:05 +0100

    DRY test

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,18 +1,17 @@
 const puppeteer = require('puppeteer');
 
-test('Adds two numbers', () => {
-    const sum = 1 + 2;
+let browser, page;
 
-    expect(sum).toEqual(3);
-});
-
-test('We can launch a browser', async () => {
-    const browser = await puppeteer.launch({
+beforeEach(async () => {
+    browser = await puppeteer.launch({
         headless: false
     });
-    const page = await browser.newPage();
+    page = await browser.newPage();
     await page.goto('localhost:3000');
+    }
+);
 
+test('We can launch a browser', async () => {
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
     expect(text).toEqual('Blogster');
