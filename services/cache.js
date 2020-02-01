7388886f882791e741a9a6c3306fd9891150f211
william <william.del.rosario@iceshop.nl>
2020-02-01 08:19:17 +0100

    cache all the users posts on one individual cache object --nested hashes

--- services/cache.js
+++ services/cache.js
@@ -4,11 +4,12 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
     this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
 
     return this;
 };
@@ -22,7 +23,7 @@ mongoose.Query.prototype.exec = async function () {
     }));
 
     // See if we have a value for 'key' in redis
-    const cacheValue = await client.get(key);
+    const cacheValue = await client.hget(this.hashKey, key);
 
     // if we do, return that
     if (cacheValue) {
@@ -36,7 +37,7 @@ mongoose.Query.prototype.exec = async function () {
 
     // Set result as a string in redis
     // expiration
-    client.set(key, JSON.stringify(result), 'EX', 10);
+    client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
     return result;
 };
