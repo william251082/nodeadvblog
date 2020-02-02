7ca1bb863a094e90595c0cc043fd789f60735480
william <william.del.rosario@iceshop.nl>
2020-02-02 11:44:41 +0100

    combining object property access

--- tests/header.test.js
+++ tests/header.test.js
@@ -79,3 +79,32 @@ allGreetings.ReferenceEvenPropertiesThatDontExist
 
 console.log(allGreetings.german);
 console.log(allGreetings.english());
+
+class Page {
+    goto() { console.log('hi')}
+    setCookie() { console.log('hi')}
+}
+
+class CustomPage {
+    static build() {
+        const page = new Page();
+        const customPage = new CustomPage(page);
+
+        const superPage = new Proxy(customPage, {
+            get: function (target, property) {
+                return target[property] || page[property]
+            }
+        });
+        return superPage;
+    }
+
+    constructor(page) {
+        this.page = page
+    }
+
+    login() {
+        this.page.goto('localhost:3000');
+        this.page.setCookie();
+    }
+}
+
