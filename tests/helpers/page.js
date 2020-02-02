0d0a3e189ca656c68e6fd87e9b79e61be0a797a5
william <william.del.rosario@iceshop.nl>
2020-02-02 20:53:33 +0100

    Default navigation

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -30,7 +30,7 @@ class CustomPage {
         // set session cookie
         await this.page.setCookie({ name: 'session', value: session });
         await this.page.setCookie({ name: 'session.sig', value: sig });
-        await this.page.goto('localhost:3000');
+        await this.page.goto('localhost:3000/blogs');
         await this.page.waitFor('a[href="/auth/logout"]');
     }
 
