45a160801838a5b4caaf2d4720b6c64eb95fde52
william <william.del.rosario@iceshop.nl>
2020-02-08 11:52:20 +0100

    asserting page response

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -39,14 +39,3 @@ module.exports = app => {
     }
   });
 };
-
-// // Make a request directly on the browser
-// fetch('/api/blogs', {
-//     method: 'POST',
-//     credentials: 'same-origin',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({ title: 'My Title', content: 'My Content' })
-// });
-
