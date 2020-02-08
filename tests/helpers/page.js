d126c124c99eefd46f6fed583e0dd68de59e54bc
william <william.del.rosario@iceshop.nl>
2020-02-08 12:43:44 +0100

    advanced test helpers part 1

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -37,6 +37,36 @@ class CustomPage {
     async getContentsOf(selector) {
         return this.page.$eval(selector, el => el.innerHTML);
     }
+
+    get(path) {
+        //arg2 of evaluate is the variable to pass inside arg1: string
+        return this.page.evaluate(
+            (_path) => {
+                return fetch(_path, {
+                    method: 'GET',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then(res => res.json());
+            }, path
+        );
+    }
+
+    post(path, data) {
+        return this.page.evaluate(
+            (_path, _data) => {
+                return fetch(_path, {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(_data)
+                }).then(res => res.json());
+            }, path, data
+        );
+    }
 }
 
 module.exports = CustomPage;
